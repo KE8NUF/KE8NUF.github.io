c36cdb5650615928e7e0ec5bb7bedddeba9762cc
Charles Millard <cmillard@thebeaconjournal.com>
2023-02-16 23:07:44 -0500

    Update sine-waves-settings.js
    True return.

--- js/sine-waves-settings.js
+++ js/sine-waves-settings.js
@@ -1,9 +1,10 @@
 var isMobile = false;
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobile|Kindle|Windows Phone|PSP|AvantGo|Atomic Web Browser|Blazer|Chrome Mobile|Dolphin|Dolfin|Doris|GO Browser|Jasmine|MicroB|Mobile Firefox|Mobile Safari|Mobile Silk|Motorola Internet Browser|NetFront|NineSky|Nokia Web Browser|Obigo|Openwave Mobile Browser|Palm Pre web browser|Polaris|PS Vita browser|Puffin|QQbrowser|SEMC Browser|Skyfire|Tear|TeaShark|UC Browser|uZard Web|wOSBrowser|Yandex.Browser mobile/i.test(navigator.userAgent) && confirm('We see you are viewing our site on a mobile device? We ask confirmation to give you the best possible experience.')) { var isMobile = true; }
+console.log(isMobile);
 
 //waves for mobile single frame.
 //if(isMobile && ($(window).width() <= 960) ) {
-if(isMobile == 'true') {
+if(isMobile == true) {
 	//if($(window).width() <= 960) {
 		function waveRender() {
 		var lastFrameTime = 8;
