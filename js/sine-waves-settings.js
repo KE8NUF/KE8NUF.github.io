7da40c21748fc3830335641ce59e6a001d042056
Charles Millard <cmillard@thebeaconjournal.com>
2023-02-18 01:22:42 -0500

    moved mobile check to index.
    moved mobile check to index.

--- js/sine-waves-settings.js
+++ js/sine-waves-settings.js
@@ -1,15 +1,10 @@
-window.navigator.userAgent;
-var isMobile = false;
-if(/Android|webOS|iPhone|iPad|iPod|CriOS|BlackBerry|IEMobile|OPiOS|Opera Mini/i.test(navigator.userAgent)) { var isMobile = true; }
-console.log(isMobile);
-
 //waves for mobile single frame.
 //if(isMobile && ($(window).width() <= 960) ) {
 // good if( navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/WebOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) ||  navigator.userAgent.match(/CriOS/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/OPiOS/i) || navigator.userAgent.match(/Opera Mini/i) || 'ontouchstart' in document.documentElement || window.navigator.msPointerEnabled && navigator.maxTouchPoints > 1 ) {
 //if(isMobile == true || 'ontouchstart' in window || window.navigator.msPointerEnabled) {
 	
 	//sample code https://www.sitepoint.com/detect-mobile-devices-jquery/
-	if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	if( /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || isMobile == true ) {
 	//if($(window).width() <= 960) {
 		function waveRender() {
 		var lastFrameTime = 8;
