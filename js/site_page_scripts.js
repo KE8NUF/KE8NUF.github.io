31ea4902ee7a6ce3bc33f2cee39ba1ea5dcefda6
Charles Millard <cmillard@thebeaconjournal.com>
2023-06-24 00:51:25 -0400

    Update site_page_scripts.js
    Content Height Size.

--- js/site_page_scripts.js
+++ js/site_page_scripts.js
@@ -409,7 +409,7 @@ function iframeReady() {
 		//equal height content script
 		//*****
 		//$(window).on('load', function() {
-		if( $(window).width() >= 750 ) {
+		if( $(window).width() >= 720px ) {
 			equalContentHeight();
 		}
 		//});
